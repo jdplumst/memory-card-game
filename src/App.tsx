8f987ee7548370fd126bae76d0b966e97b3b48d3
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-05 22:22:34 -0500

    Add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Header from './components/Header';
 import './styles/styles.css';
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <nav className="relative w-full flex flex-wrap items-center justify-center py-4 bg-red-100 shadow-lg shadow-red-200 navbar navbar-expand-lg navbar-light">
+            <h1 className='text-4xl'>Pokemon Memory Card Game</h1>
+        </nav>
+    )
+}
+
+export default Header;
