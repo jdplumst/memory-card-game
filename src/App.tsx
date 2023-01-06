9b097f0456d1a3df90ca49c7012613eb442f3508
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2023-01-05 22:19:27 -0500

    Add score and bestScore to App state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import './styles/styles.css';
 
 function App() {
+  const [score, setScore] = useState<Number>(0);
+  const [bestScore, setBestScore] = useState<Number>(0);
+
   return (
     <div className="App">
 
